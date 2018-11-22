d562b390026e8abb413113d2246511bec95854ef
ldynia <ludd@cbs.dtu.dk>
2018-11-22 14:40:43 +0100

    done !!!

--- scripts/kubernetes-master.sh
+++ scripts/kubernetes-master.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
-# Installing kubernetes admin if bin does not exist
+echo "Reset services"
 systemctl daemon-reload
 systemctl restart kubelet
 
-if ! kubeadm token list | grep abcdef.0123456789abcdef ; then
-  echo "Run kubeadm init"
-  kubeadm init --token=abcdef.0123456789abcdef --apiserver-advertise-address=$(hostname -i) --pod-network-cidr=10.244.0.0/16
+echo "Reset kubeadm"
+yes | kubeadm reset
 
-  mkdir -p $HOME/.kube
-  cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-  chown $(id -u):$(id -g) $HOME/.kube/config
+echo "Run kubeadm init"
+kubeadm init --token=abcdef.0123456789abcdef --apiserver-advertise-address=$(hostname -i) --pod-network-cidr=10.244.0.0/16
 
-  echo "Set cni networking"
-  kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-  export KUBECONFIG=$HOME/.kube/config
-fi
+echo "Copy /etc/kubernetes/admin.conf"
+mkdir -p /home/vagrant/.kube
+yes | cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+chown vagrant:vagrant /home/vagrant/.kube/config
+
+export KUBECONFIG=/home/vagrant/.kube/config
+
+echo "Set cni networking"
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
--- scripts/kubernetes-minion.sh
+++ scripts/kubernetes-minion.sh
@@ -3,5 +3,8 @@
 systemctl daemon-reload
 systemctl restart kubelet
 
+echo "Reset kubeadm"
+yes | kubeadm reset
+
 echo "Join kubernetes cluster"
 kubeadm join master:6443 --token abcdef.0123456789abcdef --discovery-token-unsafe-skip-ca-verification
